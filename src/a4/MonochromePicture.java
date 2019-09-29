60f6f6c259359529894d8746b5314a6a777748f7
hamza <hamza@10.20.79.23>
2019-09-28 21:58:48 -0400

    novice2

---
+++ src/a4/MonochromePicture.java
@@ -0,0 +1,63 @@
+package a4;
+
+public class MonochromePicture implements Picture {
+	
+	public MonochromePicture(int width, int height, Pixel value) {
+		
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Pixel getPixel(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Picture paint(int x, int y, Pixel p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Picture paint(int x, int y, Pixel p, double factor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Picture paint(int ax, int ay, int bx, int by, Pixel p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Picture paint(int ax, int ay, int bx, int by, Pixel p, double factor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Picture paint(int cx, int cy, double radius, Pixel p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Picture paint(int cx, int cy, double radius, Pixel p, double factor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
