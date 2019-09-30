34401b07140ec597eaa45168902c18d62040fb19
hamza <hamza@localhost>
2019-09-30 19:42:26 -0400

    gradient

--- src/a4/GradientPicture.java
+++ src/a4/GradientPicture.java
@@ -2,12 +2,14 @@ package a4;
 
 public class GradientPicture implements Picture {
 	
-	private int _width;
-	private int _height;
-	private Pixel _upperLeft;
-	private Pixel _upperRight;
-	private Pixel _lowerLeft;
-	private Pixel _lowerRight;
+	int _width;
+	int _height;
+	Pixel _upperLeft;
+	Pixel _upperRight;
+	Pixel _lowerLeft;
+	Pixel _lowerRight;
+	//Picture _picture;
+	//Pixel [][] _pixel_array;
 	
 	public GradientPicture(int width, int height, Pixel upper_left, Pixel upper_right, Pixel lower_left, Pixel lower_right) {
 		if (width <= 0 || height <= 0) {
@@ -17,11 +19,40 @@ public class GradientPicture implements Picture {
 			throw new IllegalArgumentException();
 		}
 		this._width = width;
-		this._height =height;
+		this._height = height;
 		this._upperLeft = upper_left;
 		this._upperRight = upper_right;
 		this._lowerLeft = lower_left;
 		this._lowerRight = lower_right;
+
+	/*	_pixel_array = new Pixel[_width][_height];
+		for (int y = 0; y < _height; y++) {
+			for (int x = 0; x < _width; x++) {
+				if (y == 0) {
+					double factorDiv = 0;
+					if (x == 0) {
+						factorDiv = 0;
+					} else {
+						factorDiv = 1 / x;
+					}
+
+					_pixel_array[x][y] = _upperLeft.blend(_upperRight, factorDiv);
+				} else {
+					Pixel beginningOfRow = _upperLeft.blend(_lowerLeft, 1 / y);
+					Pixel endOfRow = _upperRight.blend(_lowerRight, 1 / y);
+					double factorDiv = 0;
+					if (x == 0) {
+						factorDiv = 0;
+					} else {
+						factorDiv = 1 / x;
+					}
+					_pixel_array[x][y] = beginningOfRow.blend(endOfRow, factorDiv);
+				}
+			}
+		}
+
+		this._picture = new ImmutablePixelArrayPicture(_pixel_array);
+*/
 	}
 
 	@Override
@@ -42,13 +73,38 @@ public class GradientPicture implements Picture {
 		if (x < 0 || x>= _width || y < 0 || y>= _height) {
 			throw new IllegalArgumentException();
 		}
-		return null;
+		
+		double factor = ((double) y) / ((double) (_height - 1));
+		
+		Pixel begRow = _upperLeft.blend(_lowerLeft, factor);
+		Pixel endRow = _upperRight.blend(_lowerRight, factor);
+		
+		factor = ((double) x) / ((double) (_width - 1));
+		
+		
+		return begRow.blend(endRow, factor);
 	}
 
 	@Override
 	public Picture paint(int x, int y, Pixel p, double factor) {
 		// TODO Auto-generated method stub
-		return null;
+		if (x < 0 || x>= _width || y < 0 || y>= _height) {
+			throw new IllegalArgumentException();
+		}
+		if (p==null) {
+			throw new IllegalArgumentException();
+		}
+		if (factor < 0 || factor > 1.0) {
+			throw new IllegalArgumentException();
+		}
+		
+		Pixel [][] _pix_val = new Pixel [_width][_height];
+		for (int f=0; f<_width; f++) {
+			for (int d=0; d<_height; d++) {
+				_pix_val[f][d] = getPixel(f,d);
+			}
+		}
+				return (new MutablePixelArrayPicture(_pix_val)).paint(x, y, p);
 	}
 
 }
